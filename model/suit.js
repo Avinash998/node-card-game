f1308d420a0e8809e5a2efd3200ec9089c27fffc
Avinash <avinashgolu.chourasia@gmail.com>
2020-09-08 23:32:16 +0530

    suit initilized

---
+++ model/suit.js
@@ -0,0 +1,36 @@
+const {Card} = require('./card');
+
+// Suit types clubs (♣), diamonds (♦), hearts (♥) and spades (♠)
+class Suit{
+    constructor(type,color){
+        this.type = type;
+        this.color = color;
+        this.suit = this.initilise();
+    }
+    initilise(){
+        //let twoSpade = new Card("two",2,"spade","red");
+        const two = new Card("two", 2, this.type, this.color);
+        const three = new Card("three", 3, this.type, this.color);
+        const four = new Card("four", 4, this.type, this.color);
+        const five = new Card("five", 5, this.type, this.color);
+        const six = new Card("six", 6, this.type, this.color);
+        const seven = new Card("seven", 7, this.type, this.color);
+        const eight = new Card("eight", 8, this.type, this.color);
+        const nine = new Card("nine", 9, this.type, this.color);
+        const ten = new Card("ten", 10, this.type, this.color);
+        const jack = new Card("jack", 11, this.type, this.color);
+        const queen = new Card("queen", 12, this.type, this.color);
+        const king = new Card("king", 13, this.type, this.color);
+        const ace = new Card("ace", 14, this.type, this.color);
+        return [two,three,four,five,six,seven,eight,nine,ten,jack,queen,king,ace];
+    }
+    getsuit(){
+        return this.suit;
+    }
+}
+
+// let card = new Card("two", 2, this.type, this.color);
+// console.log(card);
+const suit1= new Suit("heart", "red");
+console.log(suit1);
+console.log(suit1.suit);
