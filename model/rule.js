fc5e8f0036b3e841a8419b9e9096c4053b511632
Avinash <avinashgolu.chourasia@gmail.com>
2020-09-14 20:46:59 +0530

    rules added

--- model/rule.js
+++ model/rule.js
@@ -1,38 +1,120 @@
+var _ = require('lodash');
+
 const {Player} = require('./player');
 const {Card} = require('./card');
 const {Game} = require('./game');
 class Rule{
     constructor(){
-        this.winner;
+        this.winner = {};
     }
     threeCardTrailCheck(game){
-        for(let i=0; i<game.getPlayerList().length; i++){
-            let currentPlayer = game.getPlayerList()[i];
-            let first = currentPlayer.showAllCards()[0].getValue();
-            let second = currentPlayer.showAllCards()[1].getValue();
-            let third = currentPlayer.showAllCards()[2].getValue();
-            if((first == second)&&(first == third)){
-                let value = first+second+third;
-                if( this.winner.value == undefined || this.winner.value < value){
+        console.log(game.length);
+        for(let i=0; i<game.length; i++){
+            let currentPlayer = game[i];
+            let currentCardList = currentPlayer.showAllCards();
+            // console.log(currentPlayer.name);
+            // console.log(currentPlayer.getName());
+            // console.log(currentCardList);
+            // console.log();
+            let firstCard = currentCardList[0].getValue();
+            let secondCard = currentCardList[1].getValue();
+            let thirdCard = currentCardList[2].getValue();
+            if((firstCard == secondCard)&&(firstCard == thirdCard)){
+                let combineValue = firstCard+secondCard+thirdCard;
+                if( Object.keys(this.winner).length == 0 || this.winner.value < value){
+                    console.log();
                     this.winner.name = currentPlayer.getName();
-                    this.winner.value = value;
+                    this.winner.combineValue = combineValue;
                 }      
             }
         }
-        if(this.winner.name != undefined){
+        if(!_.isEmpty(this.winner)){
+
             console.log(`Winner is ${this.winner.name}`);
         }
         else{
-            this.twoCardTrailCheck(game);
+            console.log('Call Three secuence check Method');
+            // this.threeCardSecuenceCheck(game);
+            // const g = new Game();
+            // g.start();
+            // return 1;
+        }
+    
+    }
+    threeCardSecuenceCheck(game){
+        for(let i=0; i<game.length; i++){
+            let currentPlayer = game[i];
+            let currentCardList = currentPlayer.showAllCards();
+            let sortedCard = _.sortBy(currentCardList);
+            let flag = 1;
+            let combineValue = 0;
+            for(let j=0; j<sortedCard.length-1; j++){
+                let diff = sortedCard[j+1] - sortedCard[j]
+                if(diff != 1){
+                    //some breaking condition
+                    flag =0;
+                    break;
+                }
+                combineValue += sortedCard[j];
+            }
+            if(flag){
+                combineValue += sortedCard[currentCardList.length];
+                this.winner.name = currentPlayer.getName();
+                this.winner.combineValue = combineValue;     
+            }
+
+        }
+        if(!_.isEmpty(this.winner)){
+
+            console.log(`Winner is ${this.winner.name}`);
+        }
+        else{
+            console.log('Call two trail check Method');
+            // this.threeCardSecuenceCheck(game);
+            // const g = new Game();
+            // g.start();
+            // return 1;
+        }
+
+    }
+    twoCardTrailCheck(game){
+        for(let i=0; i<game.length; i++){
+            let currentPlayer = game[i];
+            let currentCardList = currentPlayer.showAllCards();
+            // console.log(currentPlayer.name);
+            // console.log(currentPlayer.getName());
+            // console.log(currentCardList);
+            // console.log();
+            let firstCard = currentCardList[0].getValue();
+            let secondCard = currentCardList[1].getValue();
+            let thirdCard = currentCardList[2].getValue();
+            if((firstCard == secondCard)||(firstCard == thirdCard)||(secondCard == thirdCard)){
+                let combineValue = firstCard+secondCard+thirdCard;
+                if( Object.keys(this.winner).length == 0 || this.winner.value < value){
+                    console.log();
+                    this.winner.name = currentPlayer.getName();
+                    this.winner.combineValue = combineValue;
+                }      
+            }
+        }
+        if(!_.isEmpty(this.winner)){
+
+            console.log(`Winner is ${this.winner.name}`);
+        }
+        else{
+            console.log('Call Three secuence check Method');
+            // this.threeCardSecuenceCheck(game);
+            // const g = new Game();
+            // g.start();
+            // return 1;
         }
     }
-    twoCardTrailCheck(player){
-
-    }
-    threeCardSecuenceCheck(player){
-
-    }
+    
     highestTopCardCheck(player){
 
     }
 }
+
+// const game = new Game();
+// game.start();
+module.exports = {Rule};
